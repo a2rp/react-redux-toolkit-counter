b26a4ffe5efa7fef0b967d6c4fd6c19d3fa58ff4
ash1198 <ash.ranjan09@gmail.com>
2024-04-07 18:51:10 +0530

    updated react reduxtoolkit counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import styles from "./styles.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './redux/counterSlice';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [value, setValue] = useState(0);
+    const counter = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <input
+                    className={styles.decrementButton}
+                    type="button"
+                    value="-"
+                    onClick={() => dispatch(decrement())}
+                />
+                <div className={styles.value}>{counter}</div>
+                <input
+                    className={styles.incrementButton}
+                    type="button"
+                    value="+"
+                    onClick={() => dispatch(increment())}
+                />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
